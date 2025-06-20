9574847b1470f45d997fe88540d011e78e087398
abderrrrr <abderrahmandahabi@gmail.com>
2025-06-20 15:28:52 +0200

    ajout de pied de page

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer class="bg-dark text-white py-4 mt-auto">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-4 mb-3 mb-md-0">
+                <h5>Biblizone</h5>
+                <p class="small text-muted mb-0">Découvrez le monde des mots.</p>
+            </div>
+            <div class="col-md-4 mb-3 mb-md-0">
+                <h5>Liens Utiles</h5>
+                <ul class="list-unstyled">
+                    <li><a href="livres.php" class="text-white-50 text-decoration-none">Livres</a></li>
+                    <li><a href="abonnement.php" class="text-white-50 text-decoration-none">Abonnements</a></li>
+                    <li><a href="panier.php" class="text-white-50 text-decoration-none">Panier</a></li>
+                    <li><a href="login.php" class="text-white-50 text-decoration-none">Connexion</a></li>
+                    <li><a href="#" class="text-white-50 text-decoration-none">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Suivez-nous</h5>
+                <div>
+                    <a href="#" class="text-white-50 me-2"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white-50 me-2"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-white-50"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <hr class="my-4 border-secondary">
+        <p class="mb-0 text-muted small">&copy; <?php echo date('Y'); ?> Biblizone. Tous droits réservés.</p>
+    </div>
+</footer>
+
+</body>
+</html>
